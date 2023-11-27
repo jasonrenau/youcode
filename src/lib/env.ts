02face56f1b350ce3f55247774921ff656b5ac07
Jason.renault <55652225+jasonrenau@users.noreply.github.com>
2023-11-28 00:03:17 +0100

    Add auth gestion

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,11 @@
-import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    GITHUB_ID: z.string().min(1),
-    GITHUB_SECRET: z.string().min(1),
+    GITHUB_ID: z.string(),
+    GITHUB_SECRET: z.string(),
   },
   client: {},
 
